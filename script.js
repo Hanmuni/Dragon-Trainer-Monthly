b0332c287dd777eb16207e5a46975e5654a8aa1c
Hồng-Hạnh, Chu <67581292+Hanmuni@users.noreply.github.com>
2023-12-13 17:47:39 +0100

    feat: fetch articles from API

---
+++ script.js
@@ -0,0 +1,30 @@
+article = document.querySelector("#article");
+let url = "https://vanillajsacademy.com/api/dragons.json";
+
+let fetchArticles = async () => {
+  try {
+    let response = await fetch(url);
+
+    if (!response.ok) throw response;
+
+    let data = await response.json();
+    console.log("This is the API data", data);
+
+    const html = data.articles
+      .map((article) => {
+        return `
+          <h3>${article.title}</h3>
+          <p>By ${article.author}</hp>
+          <p>${article.pubdate} </p>
+          <article>${article.article}</article>
+      `;
+      })
+      .join("");
+
+    article.innerHTML = html;
+  } catch (error) {
+    console.warn(error);
+  }
+};
+
+fetchArticles();

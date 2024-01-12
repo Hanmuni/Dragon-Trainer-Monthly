3ff96afcf4ca8949c47908c0d8bfe1c3c61c4ff2
Hồng-Hạnh, Chu <67581292+Hanmuni@users.noreply.github.com>
2024-01-12 23:48:44 +0100

    feat: sanitize API data

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let article = document.querySelector("#article");
 const url = "https://vanillajsacademy.com/api/dragons.json";
 const url2 = "https://vanillajsacademy.com/api/dragons-authors.json";
 
-let fetchUrls = async () => {
+const fetchUrls = async () => {
   try {
     let responses = await Promise.all([fetch(url), fetch(url2)]);
 
@@ -14,11 +14,11 @@ let fetchUrls = async () => {
 
     displayContent(data);
   } catch (error) {
-    console.warn(error);
+    console.error(error);
   }
 };
 
-let displayContent = (data) => {
+const displayContent = (data) => {
   const articleData = data[0].articles
     .map((article) => {
       const authorData = data[1].authors.find(
@@ -38,7 +38,54 @@ let displayContent = (data) => {
     })
     .join("");
 
-  article.innerHTML = `<h1>${data[0].publication}</h1>` + articleData;
+  article.innerHTML =
+    `<h1>${cleanHTML(data[0].publication)}</h1>` + cleanHTML(articleData);
+};
+
+// Sanitizer Helper Function
+const cleanHTML = (str, nodes) => {
+  const stringToHTML = () => {
+    let parser = new DOMParser();
+    let doc = parser.parseFromString(str, "text/html");
+    return doc.body || document.createElement("body");
+  };
+
+  const removeScripts = (html) => {
+    let scripts = html.querySelectorAll("script");
+    for (let script of scripts) {
+      script.remove();
+    }
+  };
+
+  const isPossiblyDangerous = (name, value) => {
+    let val = value.replace(/\s+/g, "").toLowerCase();
+    if (["src", "href", "xlink:href"].includes(name)) {
+      if (val.includes("javascript:") || val.includes("data:")) return true;
+    }
+    if (name.startsWith("on")) return true;
+  };
+
+  const removeAttributes = (elem) => {
+    let atts = elem.attributes;
+    for (let { name, value } of atts) {
+      if (!isPossiblyDangerous(name, value)) continue;
+      elem.removeAttribute(name);
+    }
+  };
+
+  const clean = (html) => {
+    let nodes = html.children;
+    for (let node of nodes) {
+      removeAttributes(node);
+      clean(node);
+    }
+  };
+
+  let html = stringToHTML();
+  removeScripts(html);
+  clean(html);
+
+  return nodes ? html.childNodes : html.innerHTML;
 };
 
 fetchUrls();
